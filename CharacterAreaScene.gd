1ece1e80532ed42e1674e93375308722067289af
Tayo Olofintuyi <tolofi152@gmail.com>
2024-03-23 18:32:59 -0400

    I click the button and poki goes away!

---
+++ CharacterAreaScene.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var pokihere = true
+var poki = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var button = Button.new()
+	var button = get_node("eraser")
+	button.text = "Click me"
+	button.pressed.connect(self._button_pressed)
+	add_child(button)
+	poki = get_node("Poki")
+
+func _button_pressed():
+	pokihere = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(pokihere):
+		poki.visible = true
+	else:
+		poki.visible = false
+	
+	
+	
+	
