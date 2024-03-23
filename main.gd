a8a3a451a8a093b382725ef83a2f7fd418e8cd03
Sage <silerks@appstate.edu>
2024-03-23 18:59:51 -0400

    Made working title screen

---
+++ main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func new_game():
+	get_tree().change_scene_to_file("res://introScreen.tscn")
+
+func game_over():
+	$GameTimer.stop()
