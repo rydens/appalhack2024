dc5222dc9e80bce0357ca0da1c2d759fb470b06c
blaezj <blaezjib@icloud.com>
2024-03-23 20:11:02 -0400

    finished camera switching

--- scenesFolder/cameraSwap.gd
+++ scenesFolder/cameraSwap.gd
@@ -4,8 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
 func _on_camera_a_pressed():
 	get_tree().change_scene_to_file("res://scenesFolder/cameras/camera_a.tscn")
 
