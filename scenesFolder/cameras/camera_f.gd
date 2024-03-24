d4c30a35425c201832c2374b36907749173ba88b
blaezj <blaezjib@icloud.com>
2024-03-23 20:03:03 -0400

    made camera system work

--- scenesFolder/cameras/camera_f.gd
+++ scenesFolder/cameras/camera_f.gd
@@ -10,5 +10,6 @@ func _ready():
 func _process(delta):
 	pass
 
+
 func _on_show_cameras_pressed():
 	get_tree().change_scene_to_file("res://scenesFolder/camera_interface.tscn")
--- scripts/changeCamera.gd
+++ scripts/changeCamera.gd
@@ -1,4 +1,6 @@
 extends Node
 #test maybe delete?
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://control.tscn")
+
+func _on_show_cameras_pressed():
+	get_tree().change_scene_to_file("res://scenesFolder/camera_interface.tscn")
+	
