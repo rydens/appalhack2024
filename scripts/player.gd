6049df6f2e434c2837c0f4d7e43f4c131e9d2bed
Tayo Olofintuyi <tolofi152@gmail.com>
2024-03-24 05:23:44 -0400

    on the first day God said let there be light, name ONE difference between me and God

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var left_light_on = false
+var right_light_on = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +10,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var buttonL = get_node("LobbyScene/leftLightSwitch")
+	var buttonR = get_node("LobbyScene/rightLightSwitch")
+	if (buttonL.is_pressed()):
+		if( !left_light_on):
+			get_node("LobbyScene/leftLight").visible = true
+			left_light_on = true
+		else:
+			get_node("LobbyScene/leftLight").visible = false
+			get_node("LobbyScene/rightLight").visible = false
+			left_light_on = false
+	if (buttonR.is_pressed()):
+		if(!right_light_on):
+			get_node("LobbyScene/rightLight").visible = true
+			right_light_on = true
+		else:
+			get_node("LobbyScene/leftLight").visible = false
+			get_node("LobbyScene/rightLight").visible = false
+			right_light_on = false
+		
 
 func _on_show_cameras_pressed():
 	get_tree().change_scene_to_file("res://scenesFolder/camera_interface.tscn")
+
