1d52cd688b8e2c87f7d9264c12b15051bc072f55
Sage <silerks@appstate.edu>
2024-03-24 03:56:16 -0400

    Homework button moves with camera

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,8 +1,8 @@
 extends Node
 
-var incorrect
+var incorrect = 0
 #var questions
-var questionplace
+var questionplace = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
